c7b088da85dda32a543eeb6c9106aec1b7e846c7
João Vitor <71740709+Joao24820@users.noreply.github.com>
2023-01-27 21:17:07 -0300

    Create Es.js

---
+++ Es.js
@@ -0,0 +1,10 @@
+function entrar(){
+    let movimento = document.getElementById("titulo");
+    movimento.style.color = "hsl(0, 90%, 43%)"
+}
+
+function sair(){
+    let movimento = document.getElementById("titulo");
+    movimento.style.color = "hsl(0, 92%, 15%)"
+}
+
